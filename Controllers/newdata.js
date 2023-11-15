bab3a26c98824eca8dcf71a76de6468171d383cf
somodikonrad <78954961+somodikonrad@users.noreply.github.com>
2023-11-15 10:32:56 +0100

    adatrögzítés p1

--- Controllers/newdata.js
+++ Controllers/newdata.js
@@ -9,48 +9,30 @@ function getToday(){
 
 function addSteps(){
 
-    let date = document.querySelector('#date');
+    let valami = document.querySelector('#date');
     let steps = document.querySelector('#steps');
+    let date = document.querySelector('#date');
+
 
     if (date.value == "" || steps.value == 0 ){
         showMessage("Nem adtál meg minden adatot!");
     }
     else{
-        axios.get(`${serverURL}/steps/userID/eq/${loggedUser.ID}`).then(res=>{
-            let vane = false;
-            let upID = -1;
-            res.data.forEach(item => {
-                if (item.date.split('T')[0] == date.value){
-                    vane = true;
-                    upID = item.ID;
-                    return;
-                }
-            });
-            if(vane){
-                let data = {
-                    steps : steps.value	
-                }
-                axios.patch(`${serverURL}/steps/ID/eq/${upID}`, data).then((res)=>{
-                    alert('A lépésszám módosítva!');
-                    date.value = null;
-                    steps.value = 0;
-                });
-            }
-            else{
+        
                 let data = {
                     userID : loggedUser.ID,	
                     date : date.value,	
                     steps : steps.value	
                 }
 
-                axios.post(`${serverURL}/steps`, data).then((res)=>{
-                    alert('A lépésszám rögzítve!');
+                axios.post(`${serverURL}/items`, data).then((res)=>{
+                    alert('A tétel rögzítve!');
                     date.value = null;
                     steps.value = 0;
                 });
             }
-        })
+        
     }
-}
+
 
 getToday();
